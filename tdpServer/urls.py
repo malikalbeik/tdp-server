ac72f375bf35ad4a415e864c6efd5bd713dc251e
Malik Albeik <abd.beik@me.com>
2019-08-25 19:39:42 +0300

    Edit: changed all listViews to viewSets

--- tdpServer/urls.py
+++ tdpServer/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
 
 from contents import views as cv
+from blog import views as bv
+from project import views as pv
 
 
 class OptionalSlashRouter(SimpleRouter):
@@ -15,11 +17,13 @@ class OptionalSlashRouter(SimpleRouter):
 router = OptionalSlashRouter()
 
 router.register(r'contents', cv.ContentViewSet)
+router.register(r'posts', bv.PostViewSet)
+router.register(r'projects', pv.ProjectViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('api/', include('project.urls')),
-    re_path('api/', include('blog.urls')),
+    # re_path('api/', include('project.urls')),
+    # re_path('api/', include('blog.urls')), 
     re_path('api/', include(router.urls)),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls'))
 ]
